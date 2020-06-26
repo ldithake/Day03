0d7bfccffc7012d2a98615aeeefcac89d840b9a4
ldithake <ldithake@student.wethinkcode.co.za>
2020-06-26 08:44:43 +0200

    division and remainder pointers and integers

--- ft_div_mod.c
+++
@@ -1,9 +0,0 @@
-void ft_div_mod(int a, int b, int *div, int *mod);
-
-void ft_div_mod(int a, int b, int *div, int *mod)
-{
-	int division = a/b;
-	int remainder = a%b;
-	*div = division;
-	*mod = remainder;
-}
